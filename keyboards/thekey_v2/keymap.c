d80b3e9ffef5a9c75a2e9ce71918a618c7878126
Agustin Mista <agustin@mista.me>
2023-04-27 13:37:59 +0000

    Add more FN key functionality

--- keyboards/thekey_v2/keymap.c
+++ keyboards/thekey_v2/keymap.c
@@ -22,18 +22,21 @@
 // The keymap
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [0] = LAYOUT(LT(1, KC_F20), LT(2, KC_F21), LT(3, KC_F22)),
-  [1] = LAYOUT(      _______,       KC_F23,        KC_F24),
-  [2] = LAYOUT(      RGB_RMOD,      _______,       RGB_MOD),
-  [3] = LAYOUT(      RGB_VAD,       RGB_VAI,       _______),
+  [0] = LAYOUT(LT(1, KC_F13), LT(2, KC_F14), LT(3, KC_F15)),
+  [1] = LAYOUT(      _______,       KC_F16,        KC_F17),
+  [2] = LAYOUT(      KC_F18,        _______,       KC_F19),
+  [3] = LAYOUT(      KC_F20,        KC_F21,        _______),
 
 };
+
+// LED intensity
+#define LED_INTENSITY 63
 
 // Raw HID echo
 void raw_hid_receive(uint8_t *data, uint8_t length) {
 
   uint8_t hue = data[0];
-  rgblight_sethsv_noeeprom(hue, 255, rgblight_get_val());
+  rgblight_sethsv_noeeprom(hue, 255, LED_INTENSITY);
   raw_hid_send(data, length);
 }
 
@@ -49,22 +52,22 @@ uint8_t hid_buffer[HID_MSG_SIZE];
 layer_state_t layer_state_set_user(layer_state_t state) {
   switch (get_highest_layer(state)) {
   case 1:
-    rgblight_sethsv_noeeprom(63, 255, rgblight_get_val());
+    rgblight_sethsv_noeeprom(63, 255, LED_INTENSITY);
     hid_buffer[0] = 63;
     raw_hid_send(hid_buffer, HID_MSG_SIZE);
     break;
   case 2:
-    rgblight_sethsv_noeeprom(127, 255, rgblight_get_val());
+    rgblight_sethsv_noeeprom(127, 255, LED_INTENSITY);
     hid_buffer[0] = 127;
     raw_hid_send(hid_buffer, HID_MSG_SIZE);
     break;
   case 3:
-    rgblight_sethsv_noeeprom(191, 255, rgblight_get_val());
+    rgblight_sethsv_noeeprom(191, 255, LED_INTENSITY);
     hid_buffer[0] = 191;
     raw_hid_send(hid_buffer, HID_MSG_SIZE);
     break;
   default:
-    rgblight_sethsv_noeeprom(0, 255, rgblight_get_val());
+    rgblight_sethsv_noeeprom(0, 255, LED_INTENSITY);
     hid_buffer[0] = 0;
     raw_hid_send(hid_buffer, HID_MSG_SIZE);
     break;
