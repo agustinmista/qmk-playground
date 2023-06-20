f5d5bf4f141bb0bf877b7dd560afdab155952867
Agustin Mista <agustin@mista.me>
2023-06-20 18:11:24 +0000

    Fix errors after submodule update

--- keyboards/preonic/keymap.c
+++ keyboards/preonic/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OSM(MOD_LSFT),
-  KC_LCTL, KC_LGUI, BASE,    KC_LALT,  LT_LWR(KC_SPC),   LT_RSE(KC_ENT),  KC_RALT, KC_LEAD, KC_DOWN, KC_UP
+  KC_LCTL, KC_LGUI, BASE,    KC_LALT,  LT_LWR(KC_SPC),   LT_RSE(KC_ENT),  KC_RALT, QK_LEAD, KC_DOWN, KC_UP
 ),
 
 [_LOWER] = LAYOUT_preonic_2x2u(
@@ -130,22 +130,26 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
  * Leader key sequences
  */
 
-LEADER_EXTERNS();
+uint8_t old_hue;
+uint8_t old_sat;
+uint8_t old_val;
 
-void matrix_scan_user(void) {
-  LEADER_DICTIONARY() {
-    leading = false;
-    leader_end();
+void leader_start_user(void) {
+  old_hue = rgblight_get_hue();
+  old_sat = rgblight_get_sat();
+  old_val = rgblight_get_val();
+  rgblight_sethsv_noeeprom(HSV_WHITE);
+}
 
-    SEQ_ONE_KEY(KC_T) {
-      SEND_STRING(SS_LCTL(SS_LSFT("t")));
-    }
-
-    SEQ_TWO_KEYS(KC_Q, KC_Q) {
-      SEND_STRING(SS_LALT(SS_TAP(X_F4)));
-    }
-
+void leader_end_user(void) {
+  // leader + T ==> Ctrl+Shift+t
+  if (leader_sequence_one_key(KC_T)) {
+    SEND_STRING(SS_LCTL(SS_LSFT("t")));
+  // leader + Q ==> Alt+F4
+  } else if (leader_sequence_one_key(KC_Q)) {
+    SEND_STRING(SS_LALT(SS_TAP(X_F4)));
   }
+  rgblight_sethsv_noeeprom(old_hue, old_sat, old_val);
 }
 
 /*
@@ -168,19 +172,4 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     break;
   }
   return state;
-}
-
-uint8_t old_hue;
-uint8_t old_sat;
-uint8_t old_val;
-
-void leader_start(void) {
-  old_hue = rgblight_get_hue();
-  old_sat = rgblight_get_sat();
-  old_val = rgblight_get_val();
-  rgblight_sethsv_noeeprom(HSV_WHITE);
-}
-
-void leader_end(void) {
-  rgblight_sethsv_noeeprom(old_hue, old_sat, old_val);
 }
