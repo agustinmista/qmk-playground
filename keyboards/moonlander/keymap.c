92e5d9b46e16b0681b2332a4401e21a40becabaa
Agustin Mista <agustin@mista.me>
2023-07-10 20:04:42 +0000

    Minor layout tweak

--- keyboards/moonlander/keymap.c
+++ keyboards/moonlander/keymap.c
@@ -370,8 +370,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_VOLU,           KC_MNXT, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_VOLD,           KC_MPRV, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  KC_LCTL, KC_LCTL, XXXXXXX, XXXXXXX, KC_LPRN,          KC_PWR,            MU_TOGG,          KC_RPRN, XXXXXXX, XXXXXXX, KC_RCTL, KC_RCTL,
-                             LT_LOWER(KC_SPC), KC_LGUI, KC_LALT,           VSC_WHC, QK_LEAD, LT_RAISE(KC_ENT)
+  KC_LCTL, KC_LCTL, XXXXXXX, XXXXXXX, KC_LCBR,          KC_PWR,            MU_TOGG,          KC_RCBR, XXXXXXX, XXXXXXX, KC_RCTL, KC_RCTL,
+                             LT_LOWER(KC_SPC), KC_LALT, KC_LGUI,           VSC_WHC, QK_LEAD, LT_RAISE(KC_ENT)
 ),
 
 [LOWER_LAYER] = LAYOUT_moonlander(
@@ -379,7 +379,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   MEH_SPC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX, _______,           _______, XXXXXXX, KC_UNDS, KC_PLUS, XXXXXXX, XXXXXXX, KC_PIPE,
   TASK_VW, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, _______,           _______, XXXXXXX, KC_MINS, KC_EQL,  XXXXXXX, KC_COLN, KC_DQUO,
   _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,                             XXXXXXX, XXXXXXX, KC_LABK, KC_RABK, KC_QUES, _______,
-  _______, _______, XXXXXXX, XXXXXXX, KC_LCBR,          LOWER,             LOWER,            KC_RCBR, XXXXXXX, XXXXXXX, _______, _______,
+  _______, _______, XXXXXXX, XXXXXXX, KC_LBRC,          LOWER,             LOWER,            KC_RBRC, XXXXXXX, XXXXXXX, _______, _______,
                                       _______, _______, _______,           _______, _______, LT_HYPER(KC_DEL)
 ),
 
