a865bcd408fe12d07f8e4b3fa264f8b956f9a01f
Agustin Mista <agustin@mista.me>
2025-12-30 16:33:06 +0100

    Tweak thekey and add flash script

--- keyboards/thekey/default.nix
+++ keyboards/thekey/default.nix
@@ -1,5 +1,11 @@
-{ ... }:
+{ dfu-programmer }:
 {
   keyboard = "drop/thekey/v2";
   src = ./.;
+  flash = target: ''
+    echo "Flashing ${target}.hex ..."
+    ${dfu-programmer}/bin/dfu-programmer atmega32u4 erase
+    ${dfu-programmer}/bin/dfu-programmer atmega32u4 flash ${target}.hex
+    ${dfu-programmer}/bin/dfu-programmer atmega32u4 reset
+  '';
 }
